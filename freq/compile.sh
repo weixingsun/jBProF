34a56ac782faebc77fcf71d056e93c9b564608d7
weixingsun <weixing.sun@gmail.com>
2020-03-24 09:46:07 +0800

    args m=msr k=kernel

--- freq/compile.sh
+++ freq/compile.sh
@@ -1,3 +1,1 @@
 gcc freq.c -static -lcpufreq -o freq
-#ar rcs libfreq.a /usr/lib/libcpufreq.so
-#gcc freq.o -Lfreq -o freq.bin
--- freq/freq.c
+++ freq/freq.c
@@ -1,10 +1,12 @@
+#include <fcntl.h>
+#include <cpufreq.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <cpufreq.h>
 #include <sys/sysinfo.h>
+#include <unistd.h>
 
 static int GHZ = 1024*1024;
 static int MHZ = 1024;
@@ -14,19 +16,20 @@ static int MHZ = 1024;
 }
 #define rdmsr(id, var) \
 	e = pread(fd, (void *)&var, 0x08, id); \
-        AZ("While reading IA32_MPERF",e);
+	AZ("While reading IA32_MPERF",e);
 	//printf("IA32_MPERF (0xE7) is %Ld\n", (unsigned long long)var);
 
 unsigned long cpu_msr(int index, unsigned long max){
 	u_int64_t aperf0, mperf0, aperf1, mperf1;
-	int ret;
-	char *path = "/dev/cpu/1/msr\0";
-	int e, fd;
-	//unsigned long long freq;
-	fd = open(path, O_RDONLY);
+	char *path1 = "/dev/cpu/";
+	char *path2 = "/msr\0";
+	int n = strlen(path1)+strlen(path2)+1;
+	char *path=malloc(n);
+	snprintf(path, n, "%s%d%s", path1,index,path2);
+	int fd = open(path, O_RDONLY);
 	if (fd < 0) {
-		perror("Failed to open MSR");
-		printf("Failed to open %s\n", path);
+		//perror("Failed to open MSR");
+		printf("Failed to open MSR %s\n", path);
 		return 0;
 	}
 	//printf("Successfully opened %s\n", path);
@@ -35,6 +38,7 @@ unsigned long cpu_msr(int index, unsigned long max){
 	//printf("Value returned for pread at offset 0xCE is %Lx\n", freq);
 	//printf("Current frequency is %hhd\n", (int)((freq >> 8) & 0xFF));
 	
+	int e;
 	rdmsr(0xE7, mperf0);
 	rdmsr(0xE8, aperf0);
 	
@@ -43,10 +47,7 @@ unsigned long cpu_msr(int index, unsigned long max){
 	rdmsr(0xE7, mperf1);
 	rdmsr(0xE8, aperf1);
 
-	//printf("CPU%d Frequency %lu MHz\n",index, max * (aperf1-aperf0) / (mperf1-mperf0)/MHZ );
-	
-	e = close(fd);
-	if (e < 0) {
+	if (close(fd) < 0) {
 		perror("While closing FD");
 		return 0;
 	}
@@ -56,7 +57,20 @@ unsigned long cpu_api(int index){
         return cpufreq_get_freq_kernel(index);
         //printf("CPU %d Frequency  %lu \n",index, curr/MHZ );
 }
-int main(){
+int main(int argc, char* argv[]){
+	int isMSR=0;
+	int opt;
+
+        while ((opt = getopt(argc, argv, "mk")) != -1) {
+            switch (opt) {
+            case 'm': isMSR = 1; break;
+            case 'k': isMSR = 0; break;
+            default:
+                fprintf(stderr, "Usage: %s [-m/-k] \n", argv[0]);
+                exit(-1);
+            }
+        }
+
 	unsigned long min, max, curr_msr, curr_api;
 	if (cpufreq_get_hardware_limits(0, &min, &max)) {
                 fprintf(stderr, "Could not get max frequency (P0), try load cpufreq driver\n");
@@ -64,8 +78,8 @@ int main(){
         printf("CPU Frequency [%lu - %lu]GHz\n",min/MHZ, max/MHZ );
         int nprocs = get_nprocs();
         for (int i=0; i<nprocs; ++i) {
-		curr_msr = cpu_msr(i,max); 
-		//curr_api = cpu_api(i);
+		if(isMSR) curr_msr = cpu_msr(i,max); 
+		else      curr_api = cpu_api(i);
 		printf("CPU %d Frequency  %lu \n",i, curr_msr/MHZ );
 	}
 }
