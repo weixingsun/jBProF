d28008e655597ccbcfce67b5d5080d6b24d87815
weixingsun <weixing.sun@gmail.com>
2020-05-02 00:11:55 +0800

    add tuner

---
+++ tuner/opt.cc
@@ -0,0 +1,48 @@
+#include "gp/gp.h"
+#include "gp/rprop.h"
+#include "gp/cg.h"
+#include "gp/gp_utils.h"
+
+#include <cmath>
+#include <iostream>
+
+int input_dim, param_dim;
+libgp::GaussianProcess * gp;
+size_t n;
+
+void init() {
+    input_dim = 3, param_dim = 3;
+    gp = new libgp::GaussianProcess(input_dim, "CovSum ( CovSEiso, CovNoise)");
+    Eigen::VectorXd params(param_dim);
+    params << 0, 0, log(0.01);
+    gp->covf().set_loghyper(params);
+    n = 500;
+    Eigen::MatrixXd X(n, input_dim);
+    X.setRandom();
+    X = X*10;
+    Eigen::VectorXd y = gp->covf().draw_random_sample(X);
+    for(size_t i = 0; i < n; ++i) {
+        double x[input_dim];
+        for(int j = 0; j < input_dim; ++j) x[j] = X(i,j);
+        gp->add_pattern(x, y(i));
+    }
+}
+
+
+void test(){
+  Eigen::VectorXd params(param_dim);
+  params << -1, -1, -1;
+  gp->covf().set_loghyper(params);
+
+  libgp::RProp rprop;
+  rprop.init();
+  rprop.maximize(gp, 50, 0);
+
+  double c1 = gp->covf().get_loghyper()(0);
+  double c2 = gp->covf().get_loghyper()(1);
+}
+
+int main(){
+    init();
+    test();
+}
